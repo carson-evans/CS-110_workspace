e19070a76f63677386bf919538b3dfb281ab8fe0
carson-evans <carsonevans@outlook.com>
2024-09-23 12:35:47 -0400

    Wind chill equation

--- controlflow_programs/wind_chill.py
+++ controlflow_programs/wind_chill.py
@@ -2,3 +2,17 @@ import stdio
 import sys
 
 ...
+
+t = float(sys.argv[1]) # Temperature in Fahrenheit
+v = float(sys.argv[2]) # Wind speed in miles per hour
+
+# Wind chill formula
+# w = 35.74 + 0.6215t + (0.4275t − 35.75)v^0.16
+
+if t > 50 :
+    stdio.writeln("Value of t must be ≤ 50 F")
+if v <= 3 :
+    stdio.writeln("Value of v must be > 3 mph")
+else :
+    w = 35.74 + (0.6214 * t) + (0.4275 * t - 35.75) * (v ** 0.16)
+    stdio.writeln(w)

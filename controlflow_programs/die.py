a0103177da94dd7407df6bbb59e654786b8a4feb
carson-evans <carsonevans@outlook.com>
2024-09-26 11:09:59 -0400

    Random die roll visualized

--- controlflow_programs/die.py
+++ controlflow_programs/die.py
@@ -2,3 +2,35 @@ import stdio
 import stdrandom
 
 ...
+
+roll = stdrandom.uniformInt(1, 6)
+
+if roll == 1:
+    stdio.writeln("     ")
+    stdio.writeln("  *  ")
+    stdio.writeln("     ")
+
+if roll == 2:
+    stdio.writeln("*    ")
+    stdio.writeln("     ")
+    stdio.writeln("    *")
+
+if roll == 3:
+    stdio.writeln("*    ")
+    stdio.writeln("  *  ")
+    stdio.writeln("    *")
+
+if roll == 4:
+    stdio.writeln("*   *")
+    stdio.writeln("     ")
+    stdio.writeln("*   *")
+
+if roll == 5:
+    stdio.writeln("*   *")
+    stdio.writeln("  *  ")
+    stdio.writeln("*   *")
+
+if roll == 6:
+    stdio.writeln("*   *")
+    stdio.writeln("*   *")
+    stdio.writeln("*   *")

7e283e7a74602f61fd2e05e894ae827414e1aa8c
carson-evans <carsonevans@outlook.com>
2024-09-26 11:00:00 -0400

    Day of week with if

--- controlflow_programs/day_of_week.py
+++ controlflow_programs/day_of_week.py
@@ -15,4 +15,17 @@ m0 = m + 12 * ((14 - m) // 12) - 2
 dow = (d + x0 + 31 * m0 // 12) % 7
 
 # Print day of the week (0 for Sunday, 1 for Monday, and so on)
-stdio.writeln(str(int(dow)))
+if dow == 0:
+    stdio.writeln("Sunday")
+if dow == 1:
+    stdio.writeln("Monday")
+if dow == 2:
+    stdio.writeln("Tuesday")
+if dow == 3:
+    stdio.writeln("Wednesday")
+if dow == 4:
+    stdio.writeln("Thursday")
+if dow == 5:
+    stdio.writeln("Friday")
+if dow == 6:
+    stdio.writeln("Saturday")

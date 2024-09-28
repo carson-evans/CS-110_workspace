8956bdf9509320e0559e4b04c86b7e169660f7df
carson-evans <carsonevans@outlook.com>
2024-09-28 17:46:22 -0400

    check prime

--- controlflow_programs/primality_test.py
+++ controlflow_programs/primality_test.py
@@ -2,3 +2,27 @@ import stdio
 import sys
 
 ...
+
+n = int(sys.argv[1])
+
+# check for edge case
+
+if n <= 1:
+    stdio.writeln("False")
+    sys.exit()
+if n == 2:
+    stdio.writeln("True")
+    sys.exit()
+
+# Check for divisibility from 2 to sqrt(n)
+is_prime = True
+for i in range(2, n // 2 + 1):
+    if n & i == 0:
+        is_prime = False
+        break
+
+# out
+if is_prime:
+    stdio.writeln("True")
+else:
+    stdio.writeln("False")

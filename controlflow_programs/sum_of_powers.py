d1f7d67644f7dfc1cad133d2a11131ca11573592
carson-evans <carsonevans@outlook.com>
2024-09-28 17:26:35 -0400

    Sum of powers

--- controlflow_programs/sum_of_powers.py
+++ controlflow_programs/sum_of_powers.py
@@ -2,3 +2,17 @@ import stdio
 import sys
 
 ...
+
+n = int(sys.argv[1]) # upper limit
+k = int(sys.argv[2]) # power to raise
+
+# S(n,k) = 1^k + 2^k + ... + n^k
+
+sum_of_powers = 0 # init
+
+# loop through each number from 1 to n
+for i in range(1, n+1):
+    sum_of_powers += i ** k
+
+# out
+stdio.writeln(sum_of_powers)

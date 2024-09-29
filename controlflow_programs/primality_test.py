6d994d1f219650cbe195e47bf4dc9ff411ba8fc8
carson-evans <carsonevans@outlook.com>
2024-09-29 18:01:48 -0400

    FIX: Issue #9- Refactor and logic

--- controlflow_programs/primality_test.py
+++ controlflow_programs/primality_test.py
@@ -14,10 +14,17 @@ if n == 2:
     stdio.writeln("True")
     sys.exit()
 
+# Since cannot import math,
+# Calc approx sqrt of n
+i = 1
+while i * i <= n:
+    i += 1
+sqrt_n = i-1 # approx sqrt
+
 # Check for divisibility from 2 to sqrt(n)
 is_prime = True
-for i in range(2, n // 2 + 1):
-    if n & i == 0:
+for i in range(2, sqrt_n + 1):
+    if n % i == 0:
         is_prime = False
         break
 

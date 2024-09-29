fc08528b3bd92c76bc9d356cf818d7e912d699cf
carson-evans <carsonevans@outlook.com>
2024-09-29 16:48:30 -0400

    Perfect num

--- controlflow_programs/perfect_numbers.py
+++ controlflow_programs/perfect_numbers.py
@@ -2,3 +2,15 @@ import stdio
 import sys
 
 ...
+
+n = int(sys.argv[1])
+
+for k in range(2, n+1):
+    sum_divisors = 0
+    # Find all proper divisors of k
+    for i in range(1, k // 2 + 1):
+        if k % i == 0:
+            sum_divisors += i
+    # Check if k is perfect
+    if sum_divisors == k:
+        stdio.writeln(k)

272ff477224bbf2bd6ceeb07d650235d0a95325d
carson-evans <carsonevans@outlook.com>
2024-09-30 08:20:04 -0400

    Quadratic

--- controlflow_programs/quadratic.py
+++ controlflow_programs/quadratic.py
@@ -8,15 +8,20 @@ a = float(sys.argv[1])
 b = float(sys.argv[2])
 c = float(sys.argv[3])
 
-if a == 0:
-    sys.stdout.write("Value of a must not be 0\n")
-else :
-    discriminant = b ** 2 - 4 * a * c
-    if discriminant < 0: # Check if discriminant is less than 0
-        sys.stdout.write("Value of discriminant must not be negative\n")
+discriminant = float(b ** 2) - (4 * a * c)
 
-    else :
-        # Quadratic formula
-        root1 = (-b + math.sqrt(discriminant)) // (2 * a)
-        root2 = (-b - math.sqrt(discriminant)) // (2 * a)
-        sys.stdout.write(f"{root1} {root2}\n")
+if a == 0:
+    stdio.writeln("Value of a must not be 0")
+    sys.exit()
+
+if discriminant < 0: # Check if discriminant is less than 0
+    stdio.writeln("Value of discriminant must not be negative")
+    sys.exit()
+
+else:
+    # Quadratic formula
+    root1 = float((-b + math.sqrt(discriminant))) // (2 * a)
+    root2 = float((-b - math.sqrt(discriminant))) // (2 * a)
+    stdio.write(root1)
+    stdio.write(" ")
+    stdio.write(root2)

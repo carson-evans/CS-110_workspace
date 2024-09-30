981d5363ddeb4a3cf77c5d19edffcbcac356b685
carson-evans <carsonevans@outlook.com>
2024-09-30 18:25:54 -0400

    String conversion

--- controlflow_programs/factorial.py
+++ controlflow_programs/factorial.py
@@ -1,19 +1,14 @@
 import stdio
 import sys
 
-from ipp.ipp.factors import factor
-
 ...
 
 n = int(sys.argv[1])
 
-# n! = 1 * 2 * ... (n-1) * n
-# where n != 1
-
 factorial = 1 # init
 
 # loop through
-for i in range(1, n+1):
+for i in range(1, n + 1):
     factorial *= i
 
-stdio.writeln(factorial)
+stdio.writeln(str(factorial))

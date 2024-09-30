395dd92b18fd9b69fc7c0e0f8f662230737e3a71
carson-evans <carsonevans@outlook.com>
2024-09-30 12:20:54 -0400

    FIX: Change from brute force to Newton's method

--- controlflow_programs/root.py
+++ controlflow_programs/root.py
@@ -9,17 +9,12 @@ epsilon = float(sys.argv[3]) # Precision
 
 # Write to standard output the kth root of c, up to epsilon decimal places
 
-# Handle case where c is zero
-if c == 0:
-    stdio.writeln(0.0)
-    sys.exit()
+t = c
 
-# Initialize a guess
-guess = 1.0
+# Repeat as long as |1 - c/t^k| > epsilon
+while abs(1 - (c / (t ** k))) > epsilon:
+    t = t - (t ** k - c) / (k * t ** (k - 1))
+    # Set t to t - f(t) / f'(t), where f(t) = t^k - c
+    # and f'(t) = kt^(k-1)
 
-# Iterate until close enough
-while abs(guess ** k - c) > epsilon:
-    guess = ((k-1) * guess + c / (guess**(k-1))) / k
-
-# Print approx
-stdio.writeln(guess)
+stdio.writeln(t) # Write t

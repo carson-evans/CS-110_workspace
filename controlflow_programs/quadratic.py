3c52f9079fc8888a08d789355124e494f3b08fa9
carson-evans <carsonevans@outlook.com>
2024-09-30 08:42:31 -0400

    FIX: Change to float division

--- controlflow_programs/quadratic.py
+++ controlflow_programs/quadratic.py
@@ -8,7 +8,7 @@ a = float(sys.argv[1])
 b = float(sys.argv[2])
 c = float(sys.argv[3])
 
-discriminant = float(b ** 2) - (4 * a * c)
+discriminant = b ** 2 - 4 * a * c
 
 if a == 0:
     stdio.writeln("Value of a must not be 0")
@@ -20,8 +20,8 @@ if discriminant < 0: # Check if discriminant is less than 0
 
 else:
     # Quadratic formula
-    root1 = float((-b + math.sqrt(discriminant))) // (2 * a)
-    root2 = float((-b - math.sqrt(discriminant))) // (2 * a)
+    root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    root2 = (-b - math.sqrt(discriminant)) / (2 * a)
     stdio.write(root1)
     stdio.write(" ")
     stdio.write(root2)

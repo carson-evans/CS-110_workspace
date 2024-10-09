0f4060306f149cee33de5ad13b0d4b7944e5c276
carson-evans <carsonevans@outlook.com>
2024-10-09 16:00:02 -0400

    Distance and reverse

--- mozart_waltz_generator/distance.py
+++ mozart_waltz_generator/distance.py
@@ -3,3 +3,28 @@ import stdio
 import sys
 
 ...
+
+n = int(sys.argv[1])
+
+# create empty list x
+x = []
+
+# for each i ∈ [0, n)
+#append to x a float read from standard input using stdio.readFloat()
+for i in range (0, n):
+    x.append(float(stdio.readFloat()))
+
+# create a list y of floats similar to x
+y = []
+for i in range (0, n):
+    y.append(float(stdio.readFloat()))
+
+# set distance to 0.0
+distance = 0.0
+
+# for each i ∈ [0, n)
+# increment distance by (x[i] - y[i])^2
+for i in range(0, n):
+    distance += (x[i] - y[i]) ** 2
+
+stdio.write(math.sqrt(distance))
--- mozart_waltz_generator/reverse.py
+++ mozart_waltz_generator/reverse.py
@@ -1,3 +1,23 @@
 import stdio
 
 ...
+
+# Read all strings from stdin into a list 'a' use stdio.readAllStrings()
+
+a = stdio.readAllStrings()
+# set n to the size of a
+
+n = len(a)
+
+# for each i ∈ [0, n/2)
+for i in range (0, n // 2):
+    # Exchange a[i] with a[n - i - 1]
+    a[i], a[n - i -1] = a[n - i - 1], a[i]
+
+# for each i ∈ [0, n)
+for i in range (n):
+    if i < n - 1:
+        stdio.write(a[i])
+        stdio.write(" ")
+    else:
+        stdio.writeln(a[i])

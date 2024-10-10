bb3672d89945ac4c3cc6263a934a6d020769a81f
carson-evans <carsonevans@outlook.com>
2024-10-10 10:28:35 -0400

    Birthday

--- mozart_waltz_generator/birthday.py
+++ mozart_waltz_generator/birthday.py
@@ -18,12 +18,28 @@ days_per_year = 365
 trials = int(sys.argv[1])
 count = 0
 
-for t in range (0, trials):
+for t in range (trials):
     # Set birthdaysSeen to a list the size of days_per_year
     # With all elements set to false using stdarray.createID()
+    birthdaysSeen = stdarray.create1D(days_per_year, False)
+
     # Repeat forever
+    while True:
+
         # Increment count by 1
+        count += 1
+
         # Set birthday to a random int from [0, days_per_year)
+        birthday = stdrandom.uniformInt(0, days_per_year)
+
         # If birthday was seen before, break
+        if birthdaysSeen[birthday]:
+            break
+
         # else, record that we are seeing it now
+        else:
+            birthdaysSeen[birthday] = True
+
 # Write count / trials using integer division
+average_count = count // trials
+stdio.write(average_count)

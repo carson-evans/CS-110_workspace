246fac492ee5253e2da30aad8c02b9da332c0370
carson-evans <carsonevans@outlook.com>
2024-10-24 11:01:30 -0400

    Reverse

--- rsa_cryptosystem/reverse.py
+++ rsa_cryptosystem/reverse.py
@@ -12,8 +12,13 @@ def main():
 
 # Reverses a in place, ie, without creating a new list.
 def _reverse(a):
-    ...
-
+    n = len(a)
+    # for each i in [0, n/2]
+        # exchange a[i] with a[n-i-1] using temp variable
+    for i in range(0, n // 2):
+        temp = a[i]
+        a[i] = a[n - i - 1]
+        a[n - i - 1] = temp
 
 if __name__ == "__main__":
     main()

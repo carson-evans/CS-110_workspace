4094bb9887e99f42e14b685e1a801ac771df592d
carson-evans <carsonevans@outlook.com>
2024-11-05 11:00:35 -0500

    Sum of ints recursive

--- atomic_nature_of_matter/sum_of_ints.py
+++ atomic_nature_of_matter/sum_of_ints.py
@@ -10,8 +10,18 @@ def main():
 
 # Returns the sum 1 + 2 + ... + n.
 def _sumOfInts(n):
-    ...
+    """
+    :param n:
+    :return sum of integers n - 1:
+    """
 
+    # Base case
+    if n == 1:
+        return 1
+
+    # S(n) = n + S(n - 1)
+    if n > 1:
+        return n + _sumOfInts(n - 1)
 
 if __name__ == "__main__":
     main()

923a357997aa202a73b81742cb83ba71b245add2
carson-evans <carsonevans@outlook.com>
2024-11-05 11:33:29 -0500

    Bits recursive

--- atomic_nature_of_matter/bits.py
+++ atomic_nature_of_matter/bits.py
@@ -10,13 +10,26 @@ def main():
 
 # Return the number of zeros in s.
 def _zeros(s):
-    ...
-
+    # Base case
+    if s == "":
+        return 0
+    else:
+        # If first char of s is a 0,
+        if s[0] == "0":
+            return 1 + _zeros(s[1 :])
+        return _zeros(s[1 :])
 
 # Return the number of ones in s.
 def _ones(s):
-    ...
-
+    # Base case
+    if s == "":
+        return 0
+    else:
+        # If first char of s is a 1,
+        if s[0] == "1":
+            return 1 + _ones(s[1 :])
+        else:
+            return _ones(s[1 :])
 
 if __name__ == "__main__":
     main()

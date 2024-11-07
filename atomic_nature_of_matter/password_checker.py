1043e838a6ce18e9693efbd643a506b6748c85c3
carson-evans <carsonevans@outlook.com>
2024-11-07 09:18:30 -0500

    Password checker

--- atomic_nature_of_matter/password_checker.py
+++ atomic_nature_of_matter/password_checker.py
@@ -10,8 +10,31 @@ def main():
 
 # Returns True if pwd is a valid password, and False otherwise.
 def _isValid(pwd):
-    ...
+    """
+    :param pwd:
+    :return bool:
+    """
 
+    check1, check2, check3, check4, check5 = False, False, False, False, False
+
+    if len(pwd) >= 8:
+        check1 = True
+
+    for c in pwd: # For each char c in pwd
+        if c.isdigit(): # if c is a digit, set check2 to true
+            check2 = True
+        elif c.isupper(): # else if c is upper case, set check3 to true
+            check3 = True
+        elif c.islower(): # else if c is lower case, set check4 to true
+            check4 = True
+        elif not c.isalnum(): # else if c is not alphanumeric set check5 to true
+            check5 = True
+
+    # Return the logical and of check1, check2, check3, check4, and check5
+    if check1 and check2 and check3 and check4 and check5 == True:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
